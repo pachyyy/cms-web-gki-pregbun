eebcfee900c23381d9468695f0fa7f38aef9eeb6
Adriano Emmanuel <113488216+pachyyy@users.noreply.github.com>
2026-05-21 19:26:39 +0700

    add few route and placeholder page

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -4,14 +4,34 @@ import { NavUser } from '@/components/nav-user';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/react';
-import { BookOpen, Folder, LayoutGrid } from 'lucide-react';
+import { BookOpen, Folder, LayoutDashboard, UsersRound, CalendarArrowUp, HandHeart, List } from 'lucide-react';
 import AppLogo from './app-logo';
 
 const mainNavItems: NavItem[] = [
     {
         title: 'Dashboard',
         url: '/dashboard',
-        icon: LayoutGrid,
+        icon: LayoutDashboard,
+    },
+    {
+        title: 'Kepemimpinan',
+        url: '/kepemimpinan',
+        icon: UsersRound,
+    },
+    {
+        title: 'Event',
+        url: '/event',
+        icon: CalendarArrowUp,
+    },
+    {
+        title: 'Pelayanan',
+        url: '/pelayanan',
+        icon: HandHeart,
+    },
+    {
+        title: 'Komisi',
+        url: '/komisi',
+        icon: List,
     },
 ];
 
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,18 @@ Route::middleware(['auth'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
+    Route::get('kepemimpinan', function () {
+        return Inertia::render('kepemimpinan');
+    })->name('kepemimpinan');
+    Route::get('event', function () {
+        return Inertia::render('event');
+    })->name('event');
+    Route::get('pelayanan', function () {
+        return Inertia::render('pelayanan');
+    })->name('pelayanan');
+    Route::get('komisi', function () {
+        return Inertia::render('komisi');
+    })->name('komisi');
 });
 
 require __DIR__.'/settings.php';
